d558f32b58dee6bb3a9415bb4311eaab4ab01bca
Charlie Jonas <charlie@charliejonas.co.uk>
2019-12-30 20:56:16 +0000

    Add flag to configure polling interval

--- cmd/entropyd/main.go
+++ cmd/entropyd/main.go
@@ -11,15 +11,17 @@ import (
 
 func main() {
 	serverURLPtr := flag.String("url", "https://entropy.malc.org.uk/entropy/", "URL of the remote entropy server")
-	minBitsPtr := flag.Int("min", 512, "minimum amount of entropy (in bits) to call for in a single HTTP request")
-	maxBitsPtr := flag.Int("max", 8192, "maximum amount of entropy (in bits) to call for in a single HTTP request")
+	minBitsPtr := flag.Int("min", 512, "minimum amount of entropy (in bits) in a HTTP request")
+	maxBitsPtr := flag.Int("max", 8192, "maximum amount of entropy (in bits) in a HTTP request")
+	pollIntervalPtr := flag.Int("poll", 200, "interval (in milliseconds) at which to poll the kernel entropy pool")
 	flag.Parse()
 
 	cl := entropy.NewClient(*serverURLPtr, *minBitsPtr, *maxBitsPtr)
 	pl := pool.OpenPool()
 	defer pl.Cleardown()
 
-	ticker := time.NewTicker(5 * time.Second)
+	interval := time.Duration(*pollIntervalPtr)
+	ticker := time.NewTicker(interval * time.Millisecond)
 	for {
 		select {
 		case <-ticker.C:
