08b85ec8aa0773a9e9999f154dc54029fd8ad0b2
Charlie Jonas <charlie@charliejonas.co.uk>
2020-03-11 23:14:29 +0000

    Force client TLS curves and ciphers

--- pkg/entropy/client.go
+++ pkg/entropy/client.go
@@ -1,6 +1,7 @@
 package entropy
 
 import (
+	"crypto/tls"
 	"net/http"
 	"time"
 )
@@ -13,10 +14,21 @@ type EntropyClient struct {
 }
 
 func NewClient(serverURL string, minBits int, maxBits int) *EntropyClient {
+	tlsconf := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: false,
+		CurvePreferences:         []tls.CurveID{tls.X25519},
+		CipherSuites: []uint16{
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+		},
+	}
 	tr := &http.Transport{
 		MaxIdleConns:       10,
 		IdleConnTimeout:    30 * time.Second,
 		DisableCompression: true,
+		TLSClientConfig:    tlsconf,
 	}
 	cl := &http.Client{Transport: tr}
 
