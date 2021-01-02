4d2c369351748a66fb5c6ad39fb11b5120ae9bf8
Charlie Jonas <charlie@charliejonas.co.uk>
2021-01-02 22:52:17 +0000

    Use git tags to store software version

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 SHELL := bash
 .ONESHELL:
 
+VER=$(shell git describe --tags)
 GO=$(shell which go)
 GOFMT=$(GO) fmt
-GOBUILD=$(GO) build
+GOBUILD=$(GO) build -ldflags "-X main.version=$(VER)"
 
 dir:
 	@if [ ! -d bin ] ; then mkdir -p bin ; fi
@@ -15,22 +16,22 @@ build/armv7:
 	export GOOS=linux
 	export GOARCH=arm
 	export GOARM=7
-	$(GOBUILD) -o bin/entropyd-armv7 cmd/entropyd/main.go cmd/entropyd/version.go
+	$(GOBUILD) -o bin/entropyd-armv7 cmd/entropyd/main.go
 
 build/arm64:
 	export GOOS=linux
 	export GOARCH=arm64
-	$(GOBUILD) -o bin/entropyd-arm64 cmd/entropyd/main.go cmd/entropyd/version.go
+	$(GOBUILD) -o bin/entropyd-arm64 cmd/entropyd/main.go
 
 build/386:
 	export GOOS=linux
 	export GOARCH=386
-	$(GOBUILD) -o bin/entropyd-386 cmd/entropyd/main.go cmd/entropyd/version.go
+	$(GOBUILD) -o bin/entropyd-386 cmd/entropyd/main.go
 
 build/amd64:
 	export GOOS=linux
 	export GOARCH=amd64
-	$(GOBUILD) -o bin/entropyd-amd64 cmd/entropyd/main.go cmd/entropyd/version.go
+	$(GOBUILD) -o bin/entropyd-amd64 cmd/entropyd/main.go
 
 build: build/armv7 build/arm64 build/386 build/amd64
 
--- cmd/entropyd/main.go
+++ cmd/entropyd/main.go
@@ -17,9 +17,13 @@ const maxDataBytes = 1016
 // Must be no higher than that used by the server.
 const maxReqBits = maxDataBytes * 8
 
+// Software version defaults to the value below but is overridden by the compiler in Makefile.
+var version = "dev-edge"
+
 func main() {
 	checkOS()
 
+	verThenExit := flag.Bool("version", false, "print software version and exit")
 	ipv4Ptr := flag.Bool("4", false, "force the use of IPv4 for the HTTP connection")
 	ipv6Ptr := flag.Bool("6", false, "force the use of IPv6 for the HTTP connection")
 	serverURLPtr := flag.String("url", "https://entropy.malc.org.uk/entropy/", "URL of the remote entropy server")
@@ -30,8 +34,15 @@ func main() {
 	doDryRunPtr := flag.Bool("dry-run", false, "makes a request for 512 bits of entropy but writes to stdout instead of the kernel entropy pool")
 	flag.Parse()
 
-	ver := getVer().getString()
-	ua := "entropyd/" + ver + " (+https://github.com/CHTJonas/entropyd)"
+	// Print version and exit if the user asked us to.
+	if *verThenExit {
+		path := os.Args[0]
+		fmt.Println(path, "version", version)
+		os.Exit(0)
+	}
+
+	// Setup User-Agent header and IP protocol version.
+	ua := "entropyd/" + version + " (+https://github.com/CHTJonas/entropyd)"
 	ipv := ""
 	if *ipv4Ptr {
 		ipv = "tcp4"
@@ -39,6 +50,8 @@ func main() {
 	if *ipv6Ptr {
 		ipv = "tcp6"
 	}
+
+	// Instantiate the actual entropy client.
 	cl := entropy.NewClient(*serverURLPtr, *minBitsPtr, *maxBitsPtr, ua, ipv)
 	pl := pool.OpenPool()
 	defer pl.Cleardown()
